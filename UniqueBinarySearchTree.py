c0c4e28af6d7c334d76ed44001b3183bec9db237
Naveen Kumar <114800360+Naveen3251@users.noreply.github.com>
2023-07-18 11:11:50 +0530

    Leetcode96

---
+++ UniqueBinarySearchTree.py
@@ -0,0 +1,29 @@
+'''
+Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+
+ 
+
+Example 1:
+
+
+Input: n = 3
+Output: 5
+
+Example 2:
+
+Input: n = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 19
+
+'''
+class Solution:
+    def numTrees(self, n: int) -> int:
+        ans=1
+        for i in range(n+1,(2*n)+1):
+            ans*=i
+            ans//=i-n
+        return ans//(n+1)
